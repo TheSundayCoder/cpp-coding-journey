cf64faa927b4868119e1ee2fe62c43c6e5716a68
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-23 01:21:51 +0530

    Add files via upload

---
+++ 0007_Demo_enum_and_typedef.cpp
@@ -0,0 +1,47 @@
+
+// #include <iostream>
+// using namespace std;
+
+// enum day
+// {
+//     mon,  //0
+//     tue,  //1
+//     wed=69,  //69
+//     thu,  //70
+//     fri=0, //0
+//     sat, //1
+//     sun //2
+// };
+
+// int main()
+// {
+//     day d1 = wed;
+//     cout << d1 << endl; // 2
+//     int x = d1;  
+//     cout<<"***"<<x<<"***"<<endl;       // valid..x = 69 = wed
+//     day D2;
+//     int y = 5;
+//     // error  D2 = y;
+//  int z = wed;
+//  cout<<"*****"<<z<<"****"<<endl;
+//     cout<<tue<<endl<<wed<<endl<<thu<<endl<<fri<<endl<<sat<<endl<<mon; //1 and 69 and 70
+
+
+
+//     return 0;
+// }
+
+
+
+#include <iostream>
+using namespace std;
+typedef int marks;
+
+int main() {
+    marks m1 , m2 , m3;
+    m1 = 50;
+    m2 = 69 ;
+    m3 = 100;
+    cout<<(m2);
+    return 0;
+}
