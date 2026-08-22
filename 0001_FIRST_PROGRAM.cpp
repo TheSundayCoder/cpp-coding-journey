eef503c53d1b8c466d8d65345a23e7f5ab63fe38
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-22 20:24:53 +0530

    Add files via upload

---
+++ 0001_FIRST_PROGRAM.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int a,b;
+    // cout<<"enter 2 numbers"<<endl;
+    // cin>>a>>b;
+    // cout<<"addition is "<<a+b;
+
+    string n;
+    cout<<"enter name"<<endl;
+    getline(cin,n);
+   cout<<endl<<"hello "<<n;
+    return 0;
+}
