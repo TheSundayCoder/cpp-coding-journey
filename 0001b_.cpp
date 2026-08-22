73f72a4c1cad57b25ad4c7879723b91dc8ed8888
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-22 21:23:45 +0530

    Add files via upload

---
+++ 0001b_.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y = 32/7;
+    cout<<y<<endl<<sizeof(y);
+    
+    long int b = 100;
+    cout<<endl<<sizeof(b);
+    double d;
+    long double e;
+    cout<<endl<<sizeof(d)<<endl<<sizeof(e)<<endl;
+    unsigned char g;
+cout<<sizeof(g);
+    return 0;
+}
