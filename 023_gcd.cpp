1587d035494b6ea0be9499989385a428306bdc49
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-24 00:29:46 +0530

    Add files via upload

---
+++ 023_gcd.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m , n;
+    cin>>m>>n;
+    while(m!= n){
+        if(m>n){
+            m = m-n;
+        }
+        else if(m < n){
+            n = n -m;
+        }
+    }
+    cout<<"gcd = "<<m;
+    return 0;
+}
