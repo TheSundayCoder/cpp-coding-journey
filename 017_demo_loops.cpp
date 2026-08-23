016b3745e21ea1db7e748b7c3f862b958f68b6b4
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-23 21:17:12 +0530

    Add files via upload

---
+++ 017_demo_loops.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+//lec 71
+int main() {
+    // while(1){
+    //     cout<<"hello";
+    // }  //infinite loop
+    int i = 1;
+    while(i<=10){       //any int except 0 -> true.
+        cout<<i<<" hello"<<endl;
+        i++; //updation is important in loops....
+    }
+for(int i = 0 ; i < 10 ; i++){
+    cout<<"hii"<<endl;
+
+}
+    return 0;
+}
