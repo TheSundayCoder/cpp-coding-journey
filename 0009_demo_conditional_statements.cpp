c5d5ebc017546ca5b38aa8d060cd4e0b7ce413b8
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-23 13:39:26 +0530

    Add files via upload

---
+++ 0009_demo_conditional_statements.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //  if(false /*any integer except 0 = true ,,,,0 is false*/){
+    //     cout<<"hello"<<endl;
+    //  }
+    // else{
+    //     cout<<"hi";
+    // }
+  int roll;
+  cout<<"enter roll number";
+  cin>>roll;
+  if (roll>1){
+cout<<"valid roll no.";
+  }
+  else{
+    cout<<"invalid roll no.";
+  }
+
+    return 0;
+}
+
