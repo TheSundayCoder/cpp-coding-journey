cba877eca9c7cf178ae17844e7df2947da7e1faf
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-24 20:12:57 +0530

    Update 026_Maximun_element_in_array.cpp

--- 026_Maximun_element_in_array.cpp
+++ 026_Maximun_element_in_array.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int a[]= { 3,9,5,111,234,21,34,564,7,2,8,6},max = 0;
+    int a[]= { 3,9,5,111,234,21,34,564,7,2,8,6},max = a[0];
     for(auto x : a){
         if(x>max){
             max = x;
